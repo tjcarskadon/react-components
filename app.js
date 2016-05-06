f71920802aed632f9accf9b7b416a7fe074c7a1a
Tj Carskadon <tjcarskadon@gmail.com>
2016-05-06 13:28:49 -0700

    renders dynamic list

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
 // TODO
-var GroceryList = () => (
-    <ul>
-      <Cucumbers />
-      <Kale />
-    </ul>
-  );
-
 var Cucumbers = () => (
   <li>Cucumbers</li>
   );
@@ -13,5 +6,30 @@ var Cucumbers = () => (
 var Kale = () => (
   <li>Kale</li>
   );
+
+var GroceryListItem = (props) => (
+    <ul>
+      {props.items}
+    </ul>
+  );
+
+var GroceryList = () => (
+  <div>
+    <h2>Get some</h2>
+    <GroceryListItem items={addLi(groceryItems)} />
+  </div>
+    
+  );
+
+var groceryItems=['kale','cucumbers','coffee','cheeses']
+
+var addLi = (arr) => {
+  return arr.map(function(item) {
+    console.log(item)
+    return <li>{item}</li>
+  });
+} 
+
+console.log(addLi(groceryItems));
 
 ReactDOM.render(<GroceryList />,document.getElementById('app'))
