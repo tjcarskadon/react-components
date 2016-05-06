c9ee449a7a2ae64f904ee5e577f3d099bd93b0d2
Tj Carskadon <tjcarskadon@gmail.com>
2016-05-06 11:58:15 -0700

    initial commit - add Kale and Cucumbers

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // TODO
+var GroceryList = () => (
+    <ul>
+      <Cucumbers />
+      <Kale />
+    </ul>
+  );
+
+var Cucumbers = () => (
+  <li>Cucumbers</li>
+  );
+
+var Kale = () => (
+  <li>Kale</li>
+  );
+
+ReactDOM.render(<GroceryList />,document.getElementById('app'))
